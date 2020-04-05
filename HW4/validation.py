da6f87c8b0b7cb2491dde78f80c0d7d035d2ea52
ViktoriyaKazakova <45040354+ViktoriyaKazakova@users.noreply.github.com>
2020-04-05 23:00:27 +0800

    Add files via upload

---
+++ HW4/validation.py
@@ -0,0 +1,16 @@
+from sklearn.metrics import precision_score, recall_score, f1_score, log_loss, roc_auc_score
+
+
+
+def evaluation(y_true, y_pred, y_prob):
+    precision = precision_score(y_true=y_true, y_pred=y_pred)
+    recall = recall_score(y_true=y_true, y_pred=y_pred)
+    f1 = f1_score(y_true=y_true, y_pred=y_pred)
+    ll = log_loss(y_true=y_true, y_pred=y_prob)
+    roc_auc = roc_auc_score(y_true=y_true, y_score=y_prob)
+    print('Precision: {}'.format(precision))
+    print('Recall: {}'.format(recall))
+    print('F1: {}'.format(f1))
+    print('Log Loss: {}'.format(ll))
+    print('ROC AUC: {}'.format(roc_auc))
+    return precision, recall, f1, ll, roc_auc
